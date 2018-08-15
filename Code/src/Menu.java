f9fb074635093978ca92cafb05e9315bf72a0f40
borjamilcar <borjamilcar01@gmail.com>
2018-08-15 17:33:44 +0100

    Calculadora

--- Code/src/Menu.java
+++ Code/src/Menu.java
@@ -1,3 +1,14 @@
-public class Menu {
+/**
+ * Menu
+ *
+ * Classe responsável por criar o(s) menus(s)
+ *
+ * @author Rei Panda
+ * @version 1.0
+ */
 
+public class Menu {
+    public void menu() {
+        System.out.println();
+    }
 }
--- Code/src/Operador.java
+++ Code/src/Operador.java
@@ -1,2 +1,12 @@
+/**
+ * Operador
+ *
+ * Classe responsável pelas operações da calculadora
+ * Adição - Subtração - Multiplicação - Divisão - Conversor
+ *
+ * @author Rei Panda
+ * @version 1.0
+ */
+
 public class Operador {
 }
