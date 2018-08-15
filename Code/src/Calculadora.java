bfd68ae56a5f8e11e25845bc661fd50f34ea6203
borjamilcar <borjamilcar01@gmail.com>
2018-08-15 17:27:06 +0100

    Calculadora

--- Code/src/Calculadora.java
+++ Code/src/Calculadora.java
@@ -1,3 +1,13 @@
+/**
+ * Calculadora
+ *
+ * Classe principal que conecta todas as demais classes
+ * Exibe o menu  da classe Menu e chama os operadores da classe Operador
+ *
+ * @author Rei Panda
+ * @version 1.0
+ */
+
 public class Calculadora {
     public static void main(String[] args) {
         System.out.println("Hello Java");
